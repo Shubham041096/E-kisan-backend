0ca61c4fff33b34933481e257c10113864ea77e4
ShardulSevekari <141114773+shardulsevekarihexalytics@users.noreply.github.com>
2023-11-23 18:09:29 +0530

    removed security

--- Final/src/main/java/com/app/DemoApplication.java
+++ Final/src/main/java/com/app/DemoApplication.java
@@ -3,8 +3,8 @@ package com.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -13,9 +13,9 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	//configure BCryptPassword encode bean
-		@Bean
-		public PasswordEncoder encoder()
-		{
-			return new BCryptPasswordEncoder();
-		}
+//		@Bean
+//		public PasswordEncoder encoder()
+//		{
+//			return new BCryptPasswordEncoder();
+//		}
 }
